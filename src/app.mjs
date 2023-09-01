b4c51e40bc79bbc17bee8fa88ebcb8368fb42f0c
Bernardo Di Miceli <bdm.444@gmail.com>
2023-09-01 17:25:22 +0200

    saga pattern implementato

--- src/app.mjs
+++ src/app.mjs
@@ -1,13 +1,14 @@
 export const lambdaHandler = async (event, context) => {
-    try {
-        return {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'Pagamento riuscito',
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+    // try {
+    //     return {
+    //         'statusCode': 200,
+    //         'body': JSON.stringify({
+    //             message: 'Pagamento riuscito',
+    //         })
+    //     }
+    // } catch (err) {
+    //     console.log(err);
+    //     return err;
+    // }
+    throw new Error('Pagamento rifiutato')
 };
